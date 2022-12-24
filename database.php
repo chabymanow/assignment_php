1277123d52aed5024f4c8eacbbb9a70190463aab
chabymanow <chabymanow@gmail.com>
2022-12-24 16:57:45 +0000

    New again

--- database.php
+++ database.php
@@ -1,7 +1,8 @@
 <?php
+include "security_data.php";
+
     $url='127.0.0.1:3306';
-    $username='root';
-    $password='Chabyka123$';
+
     $conn=mysqli_connect($url,$username,$password,"assignment");
     if(!$conn){
     die('Could not Connect My Sql:');
